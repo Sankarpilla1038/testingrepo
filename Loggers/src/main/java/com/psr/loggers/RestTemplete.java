798d75d7a72d9a5c37d0dd0fc4debfbbfd9302ea
Your Name <you@example.com>
2025-05-17 02:03:08 +0530

    doubt commit

---
+++ Loggers/src/main/java/com/psr/loggers/RestTemplete.java
@@ -0,0 +1,15 @@
+package com.psr.loggers;
+
+public class RestTemplete {
+	RestTemplete r=new RestTemplete();
+	RestTemplete r1=new RestTemplete();
+	RestTemplete r2=new RestTemplete();
+	RestTemplete r3=new RestTemplete();
+	RestTemplete r4=new RestTemplete();
+	RestTemplete r5=new RestTemplete();
+	RestTemplete r6=new RestTemplete();
+	RestTemplete r7=new RestTemplete();
+	RestTemplete r8=new RestTemplete();
+	RestTemplete r9=new RestTemplete();
+	RestTemplete r10=new RestTemplete();
+}
